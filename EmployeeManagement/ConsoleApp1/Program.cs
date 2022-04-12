1666a537e6d4662834f82683a012fa2d822a6f1d
imj <imj.programming.2013@gmail.com>
2022-04-12 11:13:22 +1000

    99 lambdas

--- EmployeeManagement/ConsoleApp1/Program.cs
+++ EmployeeManagement/ConsoleApp1/Program.cs
@@ -1,48 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp1
 {
-    public delegate void SampleDelegate(out int number);
-
     class Program
     {
         static void Main(string[] args)
         {
-            SampleDelegate del = new SampleDelegate(SampleClass.SampleMethodOne);
-            del += SampleClass.SampleMethodTwo;
-            //del += SampleClass.SampleMethodThree;
-            //del += SampleClass.SampleMethodFour;
+            List<Employee> employees = new List<Employee>() {
+                new Employee() { ID = 101, Name = "Mary", Salary = 5000, Experience = 5 },
+                new Employee() { ID = 102, Name = "Mike", Salary = 4000, Experience = 4 },
+                new Employee() { ID = 103, Name = "JohN", Salary = 6000, Experience = 6 },
+                new Employee() { ID = 104, Name = "Todd", Salary = 3000, Experience = 3 }
+            };
 
-            int DelegateOutputParameter = -1;
-            del(out DelegateOutputParameter);
+            //Employee anonymous = employees.Find(delegate(Employee e) { return e.ID == 102 });
+            //Employee employee = employees.Find((Employee x) => x.ID == 102); //  input parameter, such that/goes to expression
+            Employee employee = employees.Find(e => e.ID == 102);
 
-            Console.WriteLine(DelegateOutputParameter); //  return thing in the method invocation list
+            Console.WriteLine(employee.ID + " " + employee.Name);
+
+            int count = employees.Count(e => e.Name.ToLower().EndsWith("n"));
+            string name = employees.Find(e => e.Name.ToLower().EndsWith("n")).Name;
+
+            Console.WriteLine(count);
+            Console.WriteLine(name);
+
+
+
         }
     }
 
-    public class SampleClass
+    class Employee
     {
-        public static void SampleMethodOne(out int number)
-        {
-            number = 1;
-        }
-
-        public static void SampleMethodTwo(out int number)
-        {
-            number = 2;
-        }
-
-        //public static void SampleMethodThree(out int number)
-        //{
-        //    number = 3;
-        //}
-
-        //public static void SampleMethodFour(out int number)
-        //{
-        //    number = 4;
-        //}
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Salary { get; set; }
+        public int Experience { get; set; }
 
     }
+
 
 }
