dd29a5c9a92289ae268caf3b5f715ba3c76562c2
imj <imj.programming.2013@gmail.com>
2022-04-16 15:08:46 +1000

    18 model

---
+++ EmployeeManagement/EmployeeManagement/Models/MockEmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository //  need interface to do dependancy injection
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee() { Id = 1, Name = "Mary", Department = "HR", Email = "mary@email.com"},
+                new Employee() { Id = 1, Name = "John", Department = "IT", Email = "john@email.com"},
+                new Employee() { Id = 1, Name = "Sam", Department = "IT", Email = "sam@email.com"},
+            };
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
